70a7d23994381c5e82783ae72a8a574d3a3a9b70
Chris Tufaro <ctufaro@gmail.com>
2018-12-13 16:52:22 -0500

    Updating Pop-ups

--- Game/js/exchange.js
+++ Game/js/exchange.js
@@ -2,6 +2,7 @@ var Exchange = function(){
     var book1tb;
     var book2tb;
     var webSocketAddr = "ws://localhost:5000/trades";
+    var orderDict = {};
     var ws;
 
     var WireEvents = function(){
@@ -20,6 +21,10 @@ var Exchange = function(){
             ws.send(JSON.stringify("restart"));
             book1tb.clear().draw();
             book2tb.clear().draw();
+        });
+
+        $('#ctBook1').click(function(){
+            console.log(orderDict);
         });
     }
 
@@ -60,36 +65,10 @@ var Exchange = function(){
                 var received_msg = evt.data;
                 var orders = JSON.parse(evt.data);
                 
-                //CLEAR OUT TABLES
-                book1tb.clear().draw();
-                book2tb.clear().draw();
-                
                 for(var i = 0; i < orders.length; i++) {
                     var order = orders[i];
-                    if(order.Side == 1){
-                        book1tb.row.add([order.Spread, order.WagerAmount, order.OrderPlaceOnTxt, order.StatusTxt]).node().id = order.OrderId;
-                        book1tb.draw(false);
-                    }
-                    else{
-                        book2tb.row.add([order.Spread, order.WagerAmount, order.OrderPlaceOnTxt, order.StatusTxt]).node().id = order.OrderId;
-                        book2tb.draw(false);
-                    }
-    
-                    if(order.StatusTxt==='FILLED'){
-                        toastr.options = {"positionClass": "toast-bottom-right","closeButton": true,
-                        "preventDuplicates": true,
-                        "preventOpenDuplicates": true};           
-                        toastr.success("Your trade has been filled");                    
-                    }
-    
-                    if(order.StatusTxt==='PARTIAL'){
-                        toastr.options = {"positionClass": "toast-bottom-right","closeButton": true, 
-                        "preventDuplicates": true,
-                        "preventOpenDuplicates": true};           
-                        toastr.info("Your trade has been partially filled");               
-                    }
+                    ProcessTrade(order)
                 }
-                //console.log(order);
 
             };
 
@@ -102,6 +81,48 @@ var Exchange = function(){
             // The browser doesn't support WebSocket
             alert("WebSocket NOT supported by your Browser!");
         }        
+    }
+
+    var ProcessTrade = function(order){
+        //in this method we will show trades that are NOT in the dictionary, we will then
+        //save the wager amount and status in the dictionary
+
+        var bk = (order.Side == 1) ? book1tb : book2tb;
+
+        if (!(order.OrderId in orderDict)){
+            orderDict[order.OrderId] = order.Status;
+            bk.row.add([order.Spread, order.WagerAmount, order.OrderPlaceOnTxt, order.StatusTxt]).node().id = order.OrderId;
+            bk.draw(false);
+            //immediately FILLED OR PARTIAL
+            if(order.Status > 0){
+                ShowToast(order.Status);
+            }
+
+        }
+        else{
+            var statChange = (order.Status == orderDict[order.OrderId]);
+            if(statChange == false){
+                //update that row, and show a popup notification                
+                bk.row("#"+order.OrderId).data([order.Spread, order.WagerAmount, order.OrderPlaceOnTxt, order.StatusTxt]).draw();
+                orderDict[order.OrderId] = order.Status;
+                ShowToast(order.Status);
+            }
+            //console.log(orderDict);
+            //if false, don't do shit
+        }
+        
+    }
+
+    var ShowToast = function(status){
+        if(status===2){
+            toastr.options = {"positionClass": "toast-bottom-right","closeButton": true};           
+            toastr.success("Your trade has been filled");                    
+        }
+
+        if(status===1){
+            toastr.options = {"positionClass": "toast-bottom-right","closeButton": true};           
+            toastr.info("Your trade has been partially filled");               
+        }
     }
 
     var Init = function(){
